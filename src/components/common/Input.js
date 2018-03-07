95078856ff0bb6a02cf54a88f5aea574bd8f9605
Juman Pak <qkrwnaks92@gmail.com>
2018-03-07 15:45:37 +0900

    65. A Focus on Passing Props

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TextInput, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText }) => {
   return (
     <View>
       <Text>{label}</Text>
+      <TextInput
+        value={value}
+        style={{ height: 20, width: 100 }}
+        onChangeText={onChangeText}
+      />
     </View>
   );
 };
 
 Input.propTypes = {
-  label: PropTypes.string
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChangeText: PropTypes.func
 }
 
 export { Input };
