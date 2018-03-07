b76f0e1e5efd5549d5e07dc46c0c3ef87e763670
Juman Pak <qkrwnaks92@gmail.com>
2018-03-07 15:54:18 +0900

    66. Making the Input Pretty

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -3,12 +3,14 @@ import { TextInput, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
 const Input = ({ label, value, onChangeText }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
         value={value}
-        style={{ height: 20, width: 100 }}
+        style={inputStyle}
         onChangeText={onChangeText}
       />
     </View>
@@ -19,6 +21,28 @@ Input.propTypes = {
   label: PropTypes.string,
   value: PropTypes.string,
   onChangeText: PropTypes.func
-}
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2,
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1,
+  },
+  containerStyle: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
+};
 
 export { Input };
